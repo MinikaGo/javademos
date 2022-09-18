660595b147f5e7ab72f33e24ca05f393fafbb6fb
Umit Sinanoğlu <umit.sinanoglu@formalistech.com>
2022-09-18 16:59:16 +0300

    Initial commit

---
+++ dataTypes/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        double sayi = 12.5;
+        sayi = -129;
+
+        char karakter = 'A';
+
+        boolean dogruMu = false;
+    }
+}
